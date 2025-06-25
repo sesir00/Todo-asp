9c1af390a9d3f951129242144242ec886d5013de
sesir00 <shishir.chapagain.999@gmail.com>
2025-06-25 17:03:37 +0545

    fixed frontend backend connection issue

--- TodoAPI/Program.cs
+++ TodoAPI/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy =>
                       {
-                          policy.WithOrigins("https://todo.shishir.com.np") // Allow your React app's origin
+                          policy.WithOrigins("http://localhost:5173") // Allow your React app's origin
                                 .AllowAnyHeader()  // Allows any header in the request
                                 .AllowAnyMethod(); // Allows GET, POST, PUT, DELETE etc.
                       });
